8c452242a2c779951b50d57bb99be2626b85efcb
Andreas <Hellberg.andreas@gmail.com>
2021-02-02 21:19:27 +0100

    endoftheday

---
+++ src/containers/SessionContainer/SessionContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import styles from "./SessionContainer.module.scss"
+const SessionContainer = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.wrapper__container}>
+        I am the container of sessions
+      </div>
+    </div>
+  )
+}
+export default SessionContainer
--- src/pages/SessionsPage/SessionsPage.jsx
+++ src/pages/SessionsPage/SessionsPage.jsx
@@ -1,11 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
 import Input from "../../components/Input/Input"
+import SessionContainer from "../../containers/SessionContainer/SessionContainer"
 import styles from "./SessionsPage.module.scss"
 
 const SessionsPage = () => {
+  const [list, setList] = useState()
+
+  const handleChange = () => {}
   return (
-    <section className={styles.sessionsSection}>
-      <Input name="Search" required={true} label={"Search"}></Input>
+    <section className={styles.section}>
+      <div>
+        <div className={styles.section__input}>
+          <Input
+            handleChange={handleChange}
+            name="Search"
+            required={true}
+            label={"Search"}
+          ></Input>
+        </div>
+        <div className={styles.section__list}>
+          <SessionContainer></SessionContainer>
+        </div>
+      </div>
     </section>
   )
 }
